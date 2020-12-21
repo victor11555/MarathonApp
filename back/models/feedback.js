f3fe8d14d666f97b2e0ab6046eaf1e780ef2d4e8
Victor <vzabrovskiu@gmail.com>
2020-12-21 19:36:51 +0300

    feedback work

--- back/models/feedback.js
+++ back/models/feedback.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 
 const feedbackSchema = new mongoose.Schema({
     marathon:{type: mongoose.Schema.Types.ObjectId, ref:'Marathon'},
+    task:{type: mongoose.Schema.Types.ObjectId, ref:'Task'},
     day: { type: Number, required: true },
-    task: { type: Number, required: true },
+    taskNumb: { type: Number, required: true },
     comment: { type: String, required: true },
     points: { type: Number, required: true },
 });
