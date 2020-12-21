431a4188a77361d4671748083cac4a1703d9f029
Victor <vzabrovskiu@gmail.com>
2020-12-21 17:31:20 +0300

    start student page

--- front/src/App.js
+++ front/src/App.js
@@ -7,6 +7,7 @@ import MainPage from './pages/MainPage/MainPage';
 import MarathonAddTask from './Company/MarathonAddTask/MarathonAddTask';
 import MarathonEdit from './Company/MarathonEdit/MarathonEdit';
 import MarathonCheck from './Company/MarathonCheck/MarathonCheck';
+import MarathonAnswer from "./Student/MarathonAnswer/MarathonAnswer";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Route exact path='/dashboard/editMarathon/:id' component={MarathonEdit} />
         <Route exact path='/dashboard/checkMarathon/:id' component={MarathonCheck} />
         <Route exact path='/dashboard/addTask/:id' component={MarathonAddTask} />
+          <Route exact path='/dashboard/answerMarathon/:id' component={MarathonAnswer} />
       </Switch>
     </>
   );
--- front/src/Student/MarathonAnswer/MarathonAnswer.js
+++ front/src/Student/MarathonAnswer/MarathonAnswer.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import {useParams} from "react-router-dom";
+import {ListGroup} from "react-bootstrap";
 
 export default function MarathonAnswer() {
 
-    return (
+    const { id } = useParams();
+    const { marathons } = JSON.parse(localStorage.getItem('user'));
+    const marathon = marathons.filter((el) => el._id === id)[0];
 
+    return (
+        <ListGroup variant="flush">
+            <ListGroup.Item>Marathon: {marathon.title}</ListGroup.Item>
+            <ListGroup.Item>
+                <ul>
+                    {marathon.tasks.map((el, index) =>
+                        <li>
+                            <div>Day{el.day}</div>
+                            <ul>
+                                {el.task.map((task, i) =>
+                                    <li>
+                                        <div>Task{i+1}</div>
+                                        <div>Description: {task.description}</div>
+                                        <form></form>
+                                    </li>)}
+                            </ul>
+                        </li>
+                    )}
+                </ul>
+            </ListGroup.Item>
+        </ListGroup>
     )
 }
